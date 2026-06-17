a581ed6539b863d8d314789eb40a790005c9ad93
qpaseo <qazcgik@gmail.com>
2026-06-17 09:22:53 +0900

    first commit

---
+++ src/main/java/com/cicd_sp_01/CicdSp01Application.java
@@ -0,0 +1,13 @@
+package com.cicd_sp_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSp01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdSp01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cicd_sp_01/CicdSp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd_sp_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSp01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
